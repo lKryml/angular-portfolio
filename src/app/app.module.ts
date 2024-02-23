48c39ce2b0f560a91ee4b568dabec630c2e8853c
Krym <gumimash@gmail.com>
2024-02-23 09:38:45 +0200

    implemented card component and communication between card/portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,10 @@ import { HomeComponent } from './home/home.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ResumeComponent } from './resume/resume.component';
 import { ContactComponent } from './contact/contact.component';
+import { ProjectCardComponent } from './project-card/project-card.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, NavComponent, HomeComponent, PortfolioComponent, ResumeComponent, ContactComponent],
+  declarations: [AppComponent, HeaderComponent, NavComponent, HomeComponent, PortfolioComponent, ResumeComponent, ContactComponent, ProjectCardComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/project-card/project-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-project-card',
+  templateUrl: './project-card.component.html',
+  styleUrls: ['./project-card.component.scss'],
+})
+export class ProjectCardComponent {
+  @Input() projectTitle: string = '';
+  @Input() projectDescription: string = '';
+  @Input() skills: string[] = [];
+}
